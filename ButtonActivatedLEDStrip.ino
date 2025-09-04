3ad894a7d082769365850d9c41d8a7b0099e1ad9
josbor1 <123982835+josbor1@users.noreply.github.com>
2025-09-04 13:52:10 -0500

    Update ButtonActivatedLEDStrip.ino
    Updated boilerplate notes from README file.

--- ButtonActivatedLEDStrip.ino
+++ ButtonActivatedLEDStrip.ino
@@ -1,11 +1,14 @@
 /* 
-  This code will control a NeoPixel-style addressable LED strip by pressing a button to select preconfigured effects.
-    Wire the strip as follow:
-    BROWN -> Arduino (or power) 5 Volts
-    GREEN -> Arduino Pin 6 (configurable below)
-    WHITE -> Arduino (or power) Ground
+This code will control a NeoPixel-style addressable LED strip by pressing a button to select preconfigured effects. Wire the strip as follow:
 
-    NOTE THAT THE LEDs HAVE POLARITY AND MUST HAVE THEIR DATA CONNECTED TO THE MALE CABLE (or Din on the strip itself)
+RED -> Arduino (or power) 5 Volts
+GREEN -> Arduino Pin 6 (configurable below)
+WHITE -> Arduino (or power) Ground
+
+If your LED strip is longer than about 1 foot, you will need to power your LEDs using the power adapter. The Arduino is not powerful enough to power multiple LEDs.
+NOTE THAT THE LEDs HAVE POLARITY AND MUST HAVE THEIR DATA CONNECTED TO THE MALE CABLE (or Din on the strip itself)
+
+Find a virtual version of this project (and see wiring information) here: https://www.tinkercad.com/things/9MeaKMZq6zi-button-activated-led-strip
 */
 
 #include <Adafruit_NeoPixel.h> // You will need to ensure that the Adafruit NeoPixel library is installed via the Arduino IDE
